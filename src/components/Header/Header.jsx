8a633fc1dfdd87ffded61fc32caaad624f0ab355
Vincent SAILLARD <vincent.saillard@hotmail.fr>
2023-11-13 18:01:15 +0100

    improving responsive with sort options

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -40,6 +40,7 @@ const Header = ({
       <header>
         <div className="container">
           <div className="firstLine">
+            {/* logo */}
             <div
               onClick={() => {
                 setConnectModal(false);
@@ -50,7 +51,9 @@ const Header = ({
                 <img src={logo} alt="vinted logo aquamarine" className="logo" />
               </Link>
             </div>
-            <div className="inputs">
+            {/* options */}
+            {/* search bar */}
+            <div className="inputs bigScreens">
               <div className="research">
                 <img
                   src={glass}
@@ -68,6 +71,7 @@ const Header = ({
               </div>
 
               {onHome && (
+                // sort options
                 <>
                   <div className="options">
                     <div className="desc">
@@ -112,10 +116,12 @@ const Header = ({
                         handleInput={handleInput}
                       />
                     </div>
-                  </div>{" "}
+                  </div>
                 </>
               )}
             </div>
+
+            {/* buttons          */}
             <div className="connect">
               {tokenState ? null : (
                 <p
@@ -181,42 +187,117 @@ const Header = ({
               )}
             </div>
           </div>
+
+          {/* burger menu activated  */}
           {openBurger && (
-            <div className="secondLine">
-              {tokenState ? null : (
-                <p
-                  className="buttons2"
-                  onClick={() => {
-                    setRegisterModal(true);
-                    setConnectModal(false);
-                  }}
-                >
-                  S'inscrire
-                </p>
-              )}
-              {tokenState ? null : (
-                <p
-                  className="buttons2"
-                  onClick={() => {
-                    setConnectModal(true);
-                    setRegisterModal(false);
-                  }}
-                >
-                  Se connecter
-                </p>
-              )}
-              {tokenState && (
-                <p
-                  className="disconnect2"
-                  onClick={() => {
-                    Cookies.remove("token");
-                    setTokenState(null);
-                  }}
-                >
-                  Se déconnecter
-                </p>
-              )}
-            </div>
+            <>
+              <div className="secondLine">
+                {tokenState ? null : (
+                  <p
+                    className="buttons2"
+                    onClick={() => {
+                      setRegisterModal(true);
+                      setConnectModal(false);
+                    }}
+                  >
+                    S'inscrire
+                  </p>
+                )}
+                {tokenState ? null : (
+                  <p
+                    className="buttons2"
+                    onClick={() => {
+                      setConnectModal(true);
+                      setRegisterModal(false);
+                    }}
+                  >
+                    Se connecter
+                  </p>
+                )}
+                {tokenState && (
+                  <p
+                    className="disconnect2"
+                    onClick={() => {
+                      Cookies.remove("token");
+                      setTokenState(null);
+                    }}
+                  >
+                    Se déconnecter
+                  </p>
+                )}
+              </div>
+
+              {/* // options on small screens
+            // options on small screens
+            // options on small screens */}
+              <div className="inputs smallScreens">
+                <div className="research">
+                  <img
+                    src={glass}
+                    alt="magnifying glass grey"
+                    className="glass"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Recherche des articles"
+                    onChange={(event) => {
+                      const value = event.target.value;
+                      setSearchQuery(value);
+                    }}
+                  />
+                </div>
+
+                {onHome && (
+                  // sort options
+                  <>
+                    <div className="options">
+                      <div className="desc">
+                        <label htmlFor="order">
+                          <p>Trier par prix :</p>
+                          <div className="slide">
+                            {orderFilter ? (
+                              <div
+                                className={`symbol ${
+                                  orderFilter ? "right" : "left"
+                                }`}
+                              >
+                                ↡
+                              </div>
+                            ) : (
+                              <div
+                                className={`symbol ${
+                                  orderFilter ? "right" : "left"
+                                }`}
+                              >
+                                ↟
+                              </div>
+                            )}
+                          </div>
+                        </label>
+                        <input
+                          type="checkbox"
+                          id="order"
+                          onChange={() => {
+                            setOrderFilter(!orderFilter);
+                            console.log(orderFilter);
+                          }}
+                        />
+                      </div>
+                      <div className="minmax">
+                        <p>Prix entre :</p>
+                        <Slider
+                          minValue={minValue}
+                          maxValue={maxValue}
+                          set_minValue={set_minValue}
+                          set_maxValue={set_maxValue}
+                          handleInput={handleInput}
+                        />
+                      </div>
+                    </div>
+                  </>
+                )}
+              </div>
+            </>
           )}
         </div>
       </header>
